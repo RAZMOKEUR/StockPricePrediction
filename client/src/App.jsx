e4da3728b3aec0911b5c92df7aafe36bd671f633
RAZMOKEUR <saber.cherifi75@gmail.com>
2022-10-09 20:06:57 +0200

    [DEV]

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,28 @@
-import './App.css'
+import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [nasdaqPrice, setNasdaqPrice] = useState(1)
 
   return (
     <div className="App">
 
       <section className='head'>
-        <h1>Stock Price Prediction</h1>
+        <h1>NASDAQ Stock Price Prediction</h1>
       </section>
 
       <section className='body'>
+
+        <h3>Nasdaq Price Actual price : <span>{nasdaqPrice}</span> </h3>
+
         <form action="">
           <h3>Search Form</h3>
           <div id='searchbar'>
             <input type="search" name="stock" id="search" />
             <button><img src="src\assets\search.png" alt="" /></button>
           </div>
+
 
 
         </form>
